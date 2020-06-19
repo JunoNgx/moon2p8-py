8e8534cc03591a3ea5b919abd2fd14b8f1a36c07
JunoNgx <juno.ngx@gmail.com>
2020-06-19 19:11:59 +0800

    Add #include function

--- moon2p8.py
+++ moon2p8.py
@@ -14,18 +14,37 @@ def printLines(lines):
 def insertToAt(ls, lx, index):
     lx[index:index] = ls
 
+def getPathFromInclude(_line):
+    return _line[10:].replace('\")', '')
+
 inputFile = sys.argv[1]
 p8File = sys.argv[2]
 
+# Process the initial input file
+# =====================================
 if not os.path.isfile(inputFile):
     print('Error: input file {} does not exist. Operation terminated.'.format(str(sys.argv[1])))
 
-    # re.search('r^',)
+with open(inputFile, 'r') as inputFile:
+    inputContent = inputFile.readlines()
+
+# Search for #include and paste snippets
+# =====================================
+lineCount = 0
+for line in inputContent:
+    strippedLine = line.replace(' ', '').replace('\n', '')
+    if re.search("^#include", strippedLine, re.IGNORECASE):
+        filePath = getPathFromInclude(strippedLine)
+        with open(filePath, 'r') as f:
+            fc = f.readlines()
+        del inputContent[lineCount]
+        insertToAt(fc, inputContent, lineCount)
+    lineCount += 1
 
 # Use temp files to acquire the main body of the code
 # =====================================
-with open(inputFile, 'r') as inputFile, open('_codeBody.moon', 'w') as mf:
-    mf.write(inputFile.read())
+with open('_codeBody.moon', 'w') as mf:
+    mf.writelines(inputContent)
 
 os.system('moonc _codeBody.moon')
 with open('_codeBody.lua', 'r') as lf:
@@ -36,25 +55,31 @@ with open('_codeBody.lua', 'r') as lf:
 with open(p8File, 'r') as p8f:
     content = p8f.readlines()
 
-position = 0
+lineCount = 0
 for line in content:
-    if line.rstrip('\n') == "__lua__":
-        code_start_line = position
-    if line.rstrip('\n') == "__gfx__":
-        code_end_line = position
+    if line.replace('\n', '') == "__lua__":
+        code_start_line = lineCount
+    if line.replace('\n', '') == "__gfx__":
+        code_end_line = lineCount
 
-    position += 1
+    lineCount += 1
+
+if not code_start_line:
+    sys.exit("Error: cannot find lua code marker in pico-8 cartridge. Please use a valid output cartridge.")
+
+if not code_end_line:
+    sys.exit("Error: cannot find gfx marker in pico-8 cartridge. Please use a valid output cartridge.")
 
 # # debug
 # print(code_start_line)
 # print(code_end_line)
 
-for i in range(code_end_line-code_start_line - 1):
+for i in range(code_end_line - code_start_line - 1):
     del content[code_start_line + 1]
 
 # Final processing
 # =====================================
-insertToAt(codeBody, content, code_start_line+1)
+insertToAt(codeBody, content, code_start_line + 1)
 writeListToFile(p8File, content)
 
 # Cleaning up temp files
@@ -62,5 +87,4 @@ writeListToFile(p8File, content)
 os.remove("_codeBody.moon")
 os.remove("_codeBody.lua")
 
-print('Lua codes injected to', p8File)
-print('Operation completed without any detected issue')
+print(f'Lua codes injected to {p8File} from {inputFile.name} without any issue detected')
